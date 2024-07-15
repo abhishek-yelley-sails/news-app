7def156094cb4d9f762c2b583667643415cf4efc
Abhishek Yelley <abhishekyelley@gmail.com>
2024-07-15 15:17:47 +0530

    add: template home

---
+++ frontend/src/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import NewsCard from "../NewsCard/NewsCard";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <div className="news-cards">
+        <NewsCard />
+      </div>
+    </>
+  );
+}
