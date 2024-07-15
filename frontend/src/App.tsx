ef6237eb1b31b457114233ad6a63e73006d1c567
Abhishek Yelley <abhishekyelley@gmail.com>
2024-07-15 15:18:14 +0530

    add: router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/RootLayout/RootLayout";
+import AuthorizedRoutes from "./components/AuthorizedRoutes/AuthorizedRoutes";
+import Home from "./components/Home/Home";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <h1>Error Root!</h1>,
+    children: [
+      {
+        path: "/",
+        element: <AuthorizedRoutes />,
+        errorElement: <h1>Error AuthRoutes!</h1>,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+            errorElement: <h1>Error Home!</h1>,
+          }
+        ]
+      }
+    ]
+  }
+]
+);
 
 export default function App() {
-
   return (
-    <h1>Test</h1>
+    <RouterProvider router={router} />
   )
 }
