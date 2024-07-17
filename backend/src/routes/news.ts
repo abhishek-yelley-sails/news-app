14976ec9eef60c213266ed276c7ad5dfa5369ce6
Abhishek Yelley <abhishekyelley@gmail.com>
2024-07-17 14:47:09 +0530

    add: route to search articles
    date range not included

--- backend/src/routes/news.ts
+++ backend/src/routes/news.ts
@@ -15,4 +15,18 @@ router.get("/top/:country", async (req, res, next) => {
   });
   res.json(response.data);
 });
+
+router.get("/search", async (req, res, next) => {
+  const search = req.query.q;
+  if (!search) {
+    return res.json({
+      articles: []
+    });
+  }
+  const response = await axios({
+    method: 'get',
+    url: `https://newsapi.org/v2/everything?q=${search}&language=en&from=2024-07-12&sortBy=publishedAt&apiKey=${process.env.API_KEY}`
+  });
+  res.json(response.data);
+});
 export default router;
